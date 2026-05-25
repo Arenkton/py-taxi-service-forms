c449d18fae342269f201d6bef407926c2c061ae5
Artem Yur <artemonodon@gmail.com>
2026-05-25 10:54:25 +0200

    'Solution'

---
+++ taxi/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+from taxi.models import Car, Driver
+
+
+class CarForm(forms.ModelForm):
+    drivers = forms.ModelMultipleChoiceField(
+        queryset=Driver.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
+
+    class Meta:
+        model = Car
+        fields = "__all__"
